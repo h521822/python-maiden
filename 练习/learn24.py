e2754dfb06d3bfc438ea9df1795dccc08c9d0116
h521822 <h521822@126.com>
2020-01-07 00:11:38 +0800

    每日练习20200107

---
+++ 练习/learn24.py
@@ -0,0 +1,9 @@
+# 倒序打印字符串
+
+
+a = 'qwer1234'
+print(a[::-1])
+for i in a[::-1]:
+  print(i)
+  
+
---
+++ 练习/learn27.py
@@ -0,0 +1,10 @@
+# 对10个数进行排序。
+
+arr = []
+for i in range(10):
+    arr.append(int(input()))
+
+arr.sort()
+print(arr)
+
+
